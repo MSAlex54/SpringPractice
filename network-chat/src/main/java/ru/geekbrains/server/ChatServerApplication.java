b8d5c283925dffb4057310f6ed0bae2dfc1b3acc
Alex <msalex54@gmail.com>
2020-05-18 13:41:34 +0300

    New initialization

--- network-chat/src/main/java/ru/geekbrains/server/ChatServerApplication.java
+++ network-chat/src/main/java/ru/geekbrains/server/ChatServerApplication.java
@@ -1,4 +1,24 @@
 package ru.geekbrains.server;
 
+import ru.geekbrains.server.auth.AuthService;
+import ru.geekbrains.server.auth.AuthServiceJdbcImpl;
+import ru.geekbrains.server.persistance.UserRepository;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ChatServerApplication {
+    public static void main(String[] args) throws SQLException {
+        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/network_chat?useUnicode=true&serverTimezone=UTC","root", "alex54");
+        UserRepository userRepository = new UserRepository(conn);
+        if (userRepository.getAllUsers().size() == 0) {
+            userRepository.insert(new User(-1, "ivan", "123"));
+            userRepository.insert(new User(-1, "petr", "345"));
+            userRepository.insert(new User(-1, "julia", "789"));
+        }
+        AuthService auth = new AuthServiceJdbcImpl(userRepository);
+        ChatServer chatServer = new ChatServer(auth);
+        chatServer.start(7777);
+    }
 }
