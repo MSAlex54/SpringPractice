5bf46f9cba17666f0a8c5e9dd63fe5ea17e92267
Alex <msalex54@gmail.com>
2020-05-18 13:47:49 +0300

    prepare for changes

--- network-chat/src/main/java/ru/geekbrains/server/ChatServerApplication.java
+++ network-chat/src/main/java/ru/geekbrains/server/ChatServerApplication.java
@@ -10,15 +10,15 @@ import java.sql.SQLException;
 
 public class ChatServerApplication {
     public static void main(String[] args) throws SQLException {
-        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/network_chat?useUnicode=true&serverTimezone=UTC","root", "alex54");
-        UserRepository userRepository = new UserRepository(conn);
-        if (userRepository.getAllUsers().size() == 0) {
-            userRepository.insert(new User(-1, "ivan", "123"));
-            userRepository.insert(new User(-1, "petr", "345"));
-            userRepository.insert(new User(-1, "julia", "789"));
-        }
-        AuthService auth = new AuthServiceJdbcImpl(userRepository);
-        ChatServer chatServer = new ChatServer(auth);
+//        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/network_chat?useUnicode=true&serverTimezone=UTC","root", "alex54");
+//        UserRepository userRepository = new UserRepository(conn);
+//        if (userRepository.getAllUsers().size() == 0) {
+//            userRepository.insert(new User(-1, "ivan", "123"));
+//            userRepository.insert(new User(-1, "petr", "345"));
+//            userRepository.insert(new User(-1, "julia", "789"));
+//        }
+//        AuthService auth = new AuthServiceJdbcImpl(userRepository);
+//        ChatServer chatServer = new ChatServer(auth);
         chatServer.start(7777);
     }
 }
