185b0926eed9877d44a0d1d347e6c6b25347ac62
Alex <msalex54@gmail.com>
2020-05-28 15:49:19 +0300

    Home work for lesson 4

--- spring-mvc-example/src/main/java/ru/geekbrains/controller/UserController.java
+++ spring-mvc-example/src/main/java/ru/geekbrains/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import ru.geekbrains.persist.entity.User;
 import ru.geekbrains.service.UserService;
 
--- spring-mvc-example/src/main/java/ru/geekbrains/persist/repo/ProductRepository.java
+++ spring-mvc-example/src/main/java/ru/geekbrains/persist/repo/ProductRepository.java
@@ -1,42 +1,23 @@
 package ru.geekbrains.persist.repo;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.geekbrains.persist.entity.Product;
 
-import java.util.ArrayList;
+import java.math.BigDecimal;
 import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicLong;
-
+import java.util.Optional;
 
 @Repository
-public class ProductRepository {
-    private final AtomicLong identityGen;
+public interface ProductRepository extends JpaRepository<Product, Long> {
 
-    private final Map<Long, Product> products;
+    List<Product> findAllByTitle(String tittle);
+    List<Product> findAllByPriceBetween(BigDecimal minPrice, BigDecimal maxPrice);
+    List<Product> findAllByPriceBefore(BigDecimal maxPrice);
+    List<Product> findAllByPriceAfter(BigDecimal minPrice);
 
-    public ProductRepository() {
-        this.identityGen = new AtomicLong(0);
-        this.products = new ConcurrentHashMap<>();
-    }
-
-    public List<Product> findAll() {
-        return new ArrayList<>(products.values());
-    }
-
-    public void save(Product product) {
-        long id = identityGen.incrementAndGet();
-        product.setId(id);
-        products.put(id, product);
-    }
-
-    public void remove (Product product){
-        products.remove(product);
-    }
-
-    public Product findById(long id) {
-        return products.get(id);
-    }
+    List<Product> findAllByTitleAndPriceBetween(String title, BigDecimal minPrice, BigDecimal maxPrice);
+    List<Product> findAllByTitleAndPriceAfter(String title, BigDecimal minPrice);
+    List<Product> findAllByTitleAndPriceBefore(String title, BigDecimal maxPrice);
 
 }
