99b7b8b933e131bfaa42b7c86492dbd719198ba9
Alex <msalex54@gmail.com>
2020-06-03 18:22:10 +0300

    Home work for lesson 6

---
+++ boot_practice/src/main/java/ru/geekbrains/boot_practice/service/ProductService.java
@@ -0,0 +1,56 @@
+package ru.geekbrains.boot_practice.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.geekbrains.boot_practice.persist.entity.Product;
+import ru.geekbrains.boot_practice.persist.repo.ProductRepository;
+
+
+import java.math.BigDecimal;
+
+
+@Service
+public class ProductService {
+
+    private ProductRepository repository;
+
+    @Autowired
+    public ProductService(ProductRepository repository) {
+        this.repository = repository;
+    }
+
+    @Transactional
+    public void save(Product product) {
+        repository.save(product);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<Product> filterAll(String title, BigDecimal minPrice, BigDecimal maxPrice, Pageable pageable) {
+        if (title.equals("") && minPrice.intValue() == 0 && maxPrice.intValue() == 0) {
+            return repository.findAll(pageable);
+        } else if (title.equals("") && minPrice.intValue() == 0 && maxPrice.intValue() != 0) {
+            return repository.findAllByPriceLessThanEqual(maxPrice,pageable);
+        } else if (title.equals("") && minPrice.intValue() != 0 && maxPrice.intValue() == 0) {
+            return repository.findAllByPriceGreaterThanEqual(minPrice,pageable);
+        } else if (title.equals("") && minPrice.intValue() != 0 && maxPrice.intValue() != 0) {
+            return repository.findAllByPriceBetween(minPrice, maxPrice,pageable);
+        } else if (!title.equals("") && minPrice.intValue() == 0 && maxPrice.intValue() == 0) {
+            return repository.findAllByTitleContains(title,pageable);
+        } else if (!title.equals("") && minPrice.intValue() == 0 && maxPrice.intValue() != 0) {
+            return repository.findAllByTitleContainsAndPriceLessThanEqual(title, maxPrice,pageable);
+        } else if (!title.equals("") && minPrice.intValue() != 0 && maxPrice.intValue() == 0) {
+            return repository.findAllByTitleContainsAndPriceGreaterThanEqual(title, minPrice,pageable);
+        } else {
+            return repository.findAllByTitleContainsAndPriceBetween(title, minPrice, maxPrice,pageable);
+        }
+    }
+    public void removeById(Long id) {
+        repository.deleteById(id);
+    }
+    public Product getById(Long id) {
+        return repository.getById(id);
+    }
+}
