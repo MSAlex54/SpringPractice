7dddc68460489fa5c269969305324efeb02ae34f
Alex <msalex54@gmail.com>
2020-06-03 18:33:39 +0300

    Home work for lesson 6

---
+++ boot_practice/src/main/java/ru/geekbrains/boot_practice/persist/entity/Product.java
@@ -0,0 +1,63 @@
+package ru.geekbrains.boot_practice.persist.entity;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String title;
+
+    @Column
+    private String description;
+
+    @Column
+    private BigDecimal price;
+
+    public Product() {
+    }
+
+    public Product(Long id, String title, String description, BigDecimal price) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+}
